97dc6004c9a77e9b505f9832c971d6e51a7e2704
vietphuon <119868163+vietphuon@users.noreply.github.com>
2026-03-15 02:55:13 +0700

    Time: 3 ms (76.25%), Space: 21 MB (33.44%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        adj = [[] for _ in range(numCourses)]
+        for c, p in prerequisites:
+            adj[p].append(c)
+        
+        # 0 = unvisited, 1 = visiting (in current path), 2 = visited (done)
+        state = [0] * numCourses
+        arr = []
+        print(adj) # [[2], [0], [1]]
+        # [0, 0, 0] -> [1, 0, 0] -> [1, 0, 1] -> [1, 1, 1] -> (!) violate
+        
+        def dfs(course) -> bool:
+            # leaf
+            if state[course] == 2:
+                return True # alr sastify preq
+            if state[course] == 1:
+                return False # violate cycle
+
+            # visit
+            state[course] = 1
+
+            # node
+            for nxt in adj[course]:
+                if not dfs(nxt):
+                    return False
+            
+            state[course] = 2
+            arr.append(course)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        
+        return arr[::-1]
