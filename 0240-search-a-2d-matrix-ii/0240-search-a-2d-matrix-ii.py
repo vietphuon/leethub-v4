e4901163902d44974fb6eede98abe548d91844d7
vietphuon <119868163+vietphuon@users.noreply.github.com>
2026-03-15 03:27:59 +0700

    Time: 195 ms (5.41%), Space: 26.5 MB (5.99%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -2,16 +2,17 @@ class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         m = len(matrix)
         n = len(matrix[0])
+        visit = matrix[0][0] - 1
         def dfs(r, c):
-            # print(r, c)
+            print(r, c)
             # leaf
-            if r >= m or c >= n or not matrix[r][c] or matrix[r][c] > target:
+            if r >= m or c >= n or matrix[r][c] == visit or matrix[r][c] > target:
                 return False
             if matrix[r][c] == target:
                 return True
             
             # visit
-            matrix[r][c] = 0
+            matrix[r][c] = visit
 
             # node
             return dfs(r+1, c) or dfs(r, c+1)
