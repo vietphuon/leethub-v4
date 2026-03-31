b718fcfeb8075ed45b649e400307f5579a4ea96c
vietphuon <119868163+vietphuon@users.noreply.github.com>
2026-04-01 00:11:01 +0700

    Time: 31 ms (98.73%), Space: 20.5 MB (35.71%) - LeetHub

---
+++ 0224-basic-calculator/0224-basic-calculator.py
@@ -0,0 +1,32 @@
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = []
+        res = 0
+        sign = 1
+        num = 0
+
+        # 1 + 2 - (2 - 3)
+        for ch in s:
+            if ch.isdigit():
+                num = num*10 + int(ch)
+            elif ch == '+':
+                res += sign * num
+                num = 0
+                sign = 1
+            elif ch == '-':
+                res += sign * num
+                num = 0
+                sign = -1
+            elif ch == '(':
+                stack.append(res) # 3
+                stack.append(sign) # -
+                res = 0
+                sign = 1
+            elif ch == ')':
+                res += sign * num # -1
+                num = res
+                sign = stack.pop() # -
+                res = stack.pop() # 3
+        
+        res += sign * num
+        return res
